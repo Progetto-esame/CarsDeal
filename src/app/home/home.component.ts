37511a682af4eda07187b61b0cfb3b8ca4c3b9f6
Mirko <m.minni.1764@vallauri.edu>
2024-05-14 20:16:01 +0200

    chiamata /getAll per la pagina home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,38 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  router : Router;
-  constructor(router: Router) {
+  url = 'http://localhost:3000/';
+
+  constructor(private router: Router) {
     this.router = router; // Assign the injected router parameter to the class property.
   }
 
   redirectRicerca(){
     this.router.navigate(['/ricerca']);
     console.log("Reindirizzamento a ricerca effettuato con successo");
+  }
+
+  ngOnInit() {
+    let _carsDiv = document.getElementById('cars');
+    fetch(`${this.url}getAll`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(async(response: Response) => {
+            console.log(await response.json());
+            response.json().then((data) => {
+              for(let i = 0; i < data.length; i++){
+                
+                let _car = document.createElement('div');
+                console.log(data[i].marca
+                );
+              }
+          })
+          .catch((error: any) => {
+            console.log(error);
+        })
+    });
   }
 }
