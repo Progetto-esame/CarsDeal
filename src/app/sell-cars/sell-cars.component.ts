3a4567bf1fb7670b25ade8bef03aaef565784a69
Mirko <m.minni.1764@vallauri.edu>
2024-06-01 11:41:12 +0200

    step 2

--- src/app/sell-cars/sell-cars.component.ts
+++ src/app/sell-cars/sell-cars.component.ts
@@ -9,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class SellCarsComponent {
 
+
+  uploadFile() {
+    const fileInput = document.getElementById('fileInput');
+
+    if(fileInput.files.length === 0) 
+      return;
+
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('http://localhost:3000/upload', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log('Success:', result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+  }
+
+
+
+  document.getElementById('uploadForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const fileInput = document.getElementById('fileInput');
+    const formData = new FormData();
+    formData.append('foo', fileInput.files[0]);
+
+    try {
+        const response = await fetch('http://localhost:3000/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            console.log('File uploaded successfully!');
+        } else {
+            console.error('File upload failed!');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
 }
